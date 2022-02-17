db36086884e86ac2a659813cf3d72abfceddb590
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-02-17 23:06:36 +0600

    final commit

--- js/bill-calculation.js
+++ js/bill-calculation.js
@@ -41,31 +41,75 @@ document.getElementById("calculate-button").addEventListener("click", function (
                             document.getElementById("balance").innerText = balance;
                         }
                         else {
-                            document.getElementById("balanceAlert").innerText = "No balance left"
+                            document.getElementById("balanceAlert").innerText = "No balance left";
                         }
                     }
 
                     else {
-                        document.getElementById("expenseAlert").innerText = "Expense is more than income"
+                        document.getElementById("expenseAlert").innerText = "Expense is more than income";
                     }
 
                 }
 
                 else {
-                    document.getElementById("clothesAlert").innerText = "invalid input"
+                    document.getElementById("clothesAlert").innerText = "invalid input";
                 }
             }
 
             else {
-                document.getElementById("rentAlert").innerText = "invalid input"
+                document.getElementById("rentAlert").innerText = "invalid input";
             }
         }
 
         else {
-            document.getElementById("foodAlert").innerText = "invalid input"
+            document.getElementById("foodAlert").innerText = "invalid input";
         }
     }
     else {
-        document.getElementById("incomeAlert").innerText = "invalid input"
+        document.getElementById("incomeAlert").innerText = "invalid input";
+    }
+})
+
+// handle save button event
+document.getElementById("save-button").addEventListener("click", function () {
+    const save = document.getElementById("save");
+    const newSaveText = save.value;
+
+    // check for valid save
+    if (parseInt(newSaveText) >= 0) {
+        document.getElementById("saveAlert").innerText = "";
+        document.getElementById("savingAmount").innerText = "";
+        document.getElementById("remainingBalance").innerText = "";
+
+        const newSave = parseInt(newSaveText);
+        const givenIncomeText = document.getElementById("inputIncome").value;
+        const givenIncome = parseFloat(givenIncomeText);
+        const savingAmount = givenIncome * (newSave / 100);
+
+        // check for valid saving amount
+        if (savingAmount >= 0 && savingAmount <= givenIncome) {
+            document.getElementById("savingAlert").innerText = "";
+
+            document.getElementById("savingAmount").innerText = savingAmount;
+
+            const balanceText = document.getElementById("balance").innerText;
+            const newBalance = parseFloat(balanceText);
+            const remainingBalance = newBalance - savingAmount;
+
+            // check for valid remaining balance
+            if (remainingBalance >= 0) {
+                document.getElementById("remainingBalanceAlert").innerText = "";
+                document.getElementById("remainingBalance").innerText = remainingBalance;
+            }
+            else {
+                document.getElementById("remainingBalanceAlert").innerText = "Remaining balance is less than saving amount";
+            }
+        }
+        else {
+            document.getElementById("savingAlert").innerText = "invalid calculation";
+        }
+    }
+    else {
+        document.getElementById("saveAlert").innerText = "invalid input";
     }
 })
